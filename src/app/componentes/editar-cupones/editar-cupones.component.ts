2a680d2aca814b592228f345a1d8860c9aebfb49
DiEGO <111940173+DiegoAJimenezD@users.noreply.github.com>
2024-10-29 02:36:16 -0500

    Formularios terminados
    Creo que se terminaron los formularios

--- src/app/componentes/editar-cupones/editar-cupones.component.ts
+++ src/app/componentes/editar-cupones/editar-cupones.component.ts
@@ -1,12 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-editar-cupones',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './editar-cupones.component.html',
   styleUrl: './editar-cupones.component.css'
 })
 export class EditarCuponesComponent {
+  editarCuponForm!: FormGroup;
+  tiposDeCupon: string[];
 
+  constructor(private formBuilder: FormBuilder) {
+   this.crearFormulario();
+   this.tiposDeCupon = ['MULTIPLE', 'UNICO'];
+  }
+
+
+
+  private crearFormulario() {
+    this.editarCuponForm = this.formBuilder.group({
+     nombre: ['', [Validators.required]],
+     codigo: ['', [Validators.required]],
+     porcentaje: ['', [Validators.required]],
+     fecha_vencimiento: ['', [Validators.required]],
+     tipo: ['', [Validators.required]],
+   });
+}
+
+public editarCupon() {
+  console.log(this.editarCuponForm.value);
+}
 }
