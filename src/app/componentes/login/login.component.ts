81bf8cbfb4b1cf276aa99a492cf7cb0a028f4c26
andyfonseca2003 <anderson.fonsecal@uqvirtual.edu.co>
2024-10-29 19:46:28 -0500

    Update Bootstrap
    Se actualizan el complemento de bootstrap

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
