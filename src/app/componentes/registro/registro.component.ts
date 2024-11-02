5b3b63bf0b64fda014192b60b3d57ab56507df65
andyfonseca2003 <anderson.fonsecal@uqvirtual.edu.co>
2024-11-02 14:27:13 -0500

    Update registro

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -1,79 +1,66 @@
 import { Component } from '@angular/core';
 import { AbstractControlOptions, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../servicios/auth/auth.service';
+import { CrearCuentaDTO } from '../../interfaces/crear-cuenta-dto';
 import Swal from 'sweetalert2';
 
-
-
 @Component({
- selector: 'app-registro',
- standalone: true,
- imports: [ReactiveFormsModule],
- templateUrl: './registro.component.html',
- styleUrl: './registro.component.css'
+  selector: 'app-registro',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './registro.component.html',
+  styleUrl: './registro.component.css'
 })
-export class RegistroComponent{
+export class RegistroComponent {
+  registroForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) {
+    this.crearFormulario();
+  }
 
- registroForm!: FormGroup;
-
-
- constructor(private formBuilder: FormBuilder, private authService: AuthService) { 
-  this.crearFormulario();
-}
-
-private crearFormulario() {
-  this.registroForm = this.formBuilder.group({
-    cedula: ['', [Validators.required]],
-    nombre: ['', [Validators.required]],
-    email: ['', [Validators.required, Validators.email]],
-    direccion: ['', [Validators.required]],
-    telefono: ['', [Validators.required, Validators.maxLength(10)]],
-    password: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(7)]],
-    confirmaPassword : ['', [Validators.required, Validators.maxLength(10), Validators.minLength(7)]]
-  },
-  { validators: this.passwordsMatchValidator } as AbstractControlOptions
-);
-}
-
-public registrar() {
-  const crearCuenta = this.registroForm.value as CrearCuentaDTO;
- 
- 
-  this.authService.crearCuenta(crearCuenta).subscribe({
-    next: (data) => {
-      Swal.fire({
-        title: 'Cuenta creada',
-        text: 'La cuenta se ha creado correctamente',
-        icon: 'success',
-        confirmButtonText: 'Aceptar'
-      })
-    },
-    error: (error) => {
-      Swal.fire({
-        title: 'Error',
-        text: error.error.respuesta,
-        icon: 'error',
-        confirmButtonText: 'Aceptar'
-      })
+  private crearFormulario() {
+    this.registroForm = this.formBuilder.group({
+      cedula: ['', [Validators.required]],
+      nombre: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      direccion: ['', [Validators.required]],
+      telefono: ['', [Validators.required, Validators.maxLength(10)]]
+      // password: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(7)]],
+      // confirmaPassword: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(7)]]
     }
-  });
- 
- 
- }
- 
+      // { validators: this.passwordsMatchValidator } as AbstractControlOptions
+    );
+  }
 
-passwordsMatchValidator(formGroup: FormGroup) {
-  const password = formGroup.get('password')?.value;
-  const confirmaPassword = formGroup.get('confirmaPassword')?.value;
- 
- 
-  // Si las contraseñas no coinciden, devuelve un error, de lo contrario, null
-  return password == confirmaPassword ? null : { passwordsMismatch: true };
- }
- 
+  public registrar() {
+    console.log(this.registroForm.value);
+    const crearCuenta = this.registroForm.value as CrearCuentaDTO;
 
+    this.authService.crearCuenta(crearCuenta).subscribe({
+      next: (data) => {
+        Swal.fire({
+          title: 'Cuenta creada',
+          text: 'La cuenta se ha creado correctamente',
+          icon: 'success',
+          confirmButtonText: 'Aceptar'
+        })
+      },
+      error: (error) => {
+        Swal.fire({
+          title: 'Error',
+          text: error.error.respuesta,
+          icon: 'error',
+          confirmButtonText: 'Aceptar'
+        })
+      }
+    });
+  }
+
+  passwordsMatchValidator(formGroup: FormGroup) {
+    const password = formGroup.get('password')?.value;
+    const confirmaPassword = formGroup.get('confirmaPassword')?.value;
+
+    // Si las contraseñas no coinciden, devuelve un error, de lo contrario, null
+    return password == confirmaPassword ? null : { passwordsMismatch: true };
+  }
 }
-
-
- 
