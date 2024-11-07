607e87d8cb1d71cff8e98db33b078d5b41bc9900
andyfonseca2003 <anderson.fonsecal@uqvirtual.edu.co>
2024-11-06 21:30:22 -0500

    Update evento
    Se actualiza el inicio y el detalle evento

--- src/app/componentes/info-evento/info-evento.component.ts
+++ src/app/componentes/info-evento/info-evento.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { EventoService } from '../../servicios/evento.service';
+import Swal from 'sweetalert2';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-info-evento',
@@ -8,5 +11,31 @@ import { Component } from '@angular/core';
   styleUrl: './info-evento.component.css'
 })
 export class InfoEventoComponent {
+  item: any;
+  constructor(
+    private eventoService: EventoService,
+    private activatedRoute: ActivatedRoute // Inyectar ActivatedRoute
+  ){
+  }
 
+  ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.paramMap.get('id'); // Obtener el id de la URL
+    if (id) {
+      this.getEvent(id); // Llamar al método con el id
+    }
+  }
+
+  public getEvent(id:string) {
+    this.eventoService.getEvent(id).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.item = data.respuesta;
+      },
+      error: (error) => {
+        Swal.fire(error.respuesta)
+        console.log(error);
+      },
+    });
+
+  }
 }
